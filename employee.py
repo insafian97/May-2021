c2cb741ecc10028c770c9c73b7b5e8bf4c6340d6
Hamzah <pakhaxxer420@gmail.com>
2021-05-24 07:45:42 -0400

    Initial commit

---
+++ employee.py
@@ -0,0 +1,17 @@
+def add(first_num, second_num):
+    ans = first_num + second_num
+    return ans
+
+
+def subtract(first_num, second_num):
+    ans = first_num - second_num
+    return ans
+
+
+def multiply(first_num, second_num):
+    ans = first_num * second_num
+
+
+def divide(first_num, second_num):
+    ans = first_num / second_num
+    return ans
